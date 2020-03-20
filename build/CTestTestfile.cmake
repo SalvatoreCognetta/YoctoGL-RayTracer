0f73baf67e1e4fc09ff355f490ff14b9498aefca
SalvatoreCognetta <salvatore.cognetta@gmail.com>
2020-03-20 12:50:00 +0100

    Init commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/salvatore/Documenti/ComputerGraphics/01_raytrace_out
+# Build directory: /home/salvatore/Documenti/ComputerGraphics/01_raytrace_out/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
