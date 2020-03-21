5c9460b0ba7ecbe8cf72ac3231d7dcdd65a597ca
SalvatoreCognetta <salvatore.cognetta@gmail.com>
2020-03-21 21:48:31 +0100

    Rimossi cmake files inutili.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/salvatore/Documenti/ComputerGraphics/01_raytrace_out
-# Build directory: /home/salvatore/Documenti/ComputerGraphics/01_raytrace_out/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
